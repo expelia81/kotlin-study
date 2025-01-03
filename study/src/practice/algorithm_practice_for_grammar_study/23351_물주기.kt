7eefaa9555031d805d5e9d3aebc7e878a62ed42b
김창훈 <ch.kim@okestro.com>
2025-01-03 20:12:15 +0900

    [kotlin study] 알고리즘

--- study/src/practice/algorithm_practice_for_grammar_study/23351_물주기.kt
+++ study/src/practice/algorithm_practice_for_grammar_study/23351_물주기.kt
@@ -13,36 +13,69 @@ fun main() {
 
     var n = st.nextToken().toInt()
     var k = st.nextToken().toInt()
-    var a = st.nextToken().toInt()
-    var b = st.nextToken().toInt()
 
-    var array = Array(n+1) {k}
+    st = StringTokenizer(br.readLine(), " ")
 
-    var time = 0
-    var index = 1
-    while (true) {
-        time++
-        // a번만큼 b씩 물을 준다. 최대한 균등히 줘야하므로, 순차적으로 돌면 된다.
-        repeat(a) {
-            if (index > n) {
-                index = 1
-            }
-            array[index] += b
-        }
+    var count = st.nextToken().toInt()
 
-        // 모든 화분의 수명이 1일씩 줄어든다.
-        for (i in 1..n) {
-            array[i]--;
-            // 0인 화분이 생기면 종료한다.
-            if (array[i]==0) {
-                println(time)
-                return
-            }
-        }
+    var bar = Bar(1, n, k)
+
+    while (count-- > 0) {
+        st = StringTokenizer(br.readLine(), " ")
+        var index = st.nextToken().toInt()
+        var temp = bar.move(index)
+//        println("temp: $temp")
+        bar.result += temp
     }
+
+    bw.write(bar.result.toString())
 
 
     bw.flush()
     br.close()
     bw.close()
+}
+
+class Bar(val lMax:Int, val rMax:Int, var length:Int) {
+
+    var l = 1
+    var r = length
+    var result = 0
+
+    fun move(index:Int) : Int {
+        if (index < l) {
+            var temp = l-index
+            l = index
+            r = index+length-1
+//            println("l: $l, r: $r, index: $index")
+            return temp
+        } else if (index > r) {
+            var temp = index-r
+            r = index
+            l = index-length+1
+//            println("l: $l, r: $r, index: $index")
+            return temp
+        } else {
+//            println("l: $l, r: $r, index: $index")
+            return 0
+        }
+    }
+
+    fun moveLeft() : Boolean {
+        if (r==rMax) {
+            return false
+        }
+        l++
+        r++
+        return true
+    }
+
+    fun moveRight() : Boolean {
+        if (l==lMax) {
+            return false
+        }
+        l--
+        r--
+        return true
+    }
 }
